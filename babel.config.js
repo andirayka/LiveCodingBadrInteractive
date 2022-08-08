508bf2e3e82a17e84b424f9f3a0cb8fb65069609
andi.rayka <andi.rayka@gmail.com>
2022-08-08 23:26:13 +0700

    Set typescrite configuration

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          tests: ['./tests/'],
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@constants': './src/constants',
+          '@screens': './src/screens',
+          '@context': './src/context',
+          '@utils': './src/utils',
+          '@routes': './src/routes',
+        },
+      },
+    ],
+  ],
 };
