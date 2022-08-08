8b2c210740861fbe2cb439fbcd702a9161cb0637
andi.rayka <andi.rayka@gmail.com>
2022-08-09 06:08:52 +0700

    install react navigation stack

--- App.tsx
+++ App.tsx
@@ -1,3 +1,6 @@
+// react-native-gesture-handler should be at the top and there's nothing else before it
+import 'react-native-gesture-handler';
+
 import React, {FC} from 'react';
 import {Text} from 'react-native';
 
